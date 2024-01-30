490ec202cae202d94a9e876bb16395034a0214a4
Vikash Nayan <115102517+vikashnayan07@users.noreply.github.com>
2024-01-30 11:58:31 +0530

    Update 01_variable.js

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,7 +1,8 @@
 const accountId =24748
 let email = "vikash@abc.com"
-var city =" patna"
+var city =" patna" // usally we are not use var variable nowdays , it come to notice that it arise scope problem
 accountname = "vikash"
-// let accoutnDet
+// let accoutnDet => yadi ham koi variable declare kar le but usme value put nhi krte hai to woh  mujhe undefined message deti hai;
+
 console.log(accountId)
 console.table([email,city,accountname,])
